63cd8f412c5957f181bd2a0038b782e6f9fbb05d
cvasquez23 <vasqu1cr@cmich.edu>
2020-09-22 05:20:00 -0400

    22.09.2020

--- front-page.php
+++ front-page.php
@@ -11,7 +11,7 @@
 
     <!-- Hero Image Area -->
     <section id="home" class="container-fluid p-0">
-      <div class="header fadeIn" style="background-image: linear-gradient(rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0.55)), url('<?php header_image(); ?>')">
+      <div class="header fadeIn">
         <div class="display-3">
           <h1> <?php bloginfo('name'); ?> </h1>
           <p class="lead"> <?php bloginfo('description'); ?> </p>
@@ -24,7 +24,7 @@
       <div class="row justify-content-center">
         <!-- About Picture -->
         <div class="col-lg-6 selfie mx-auto p-0">
-          <img src="http://localhost:8080/giem-photography/wp-content/uploads/2020/06/about-pic.jpg" alt="Kim Giem and her husband">
+          <img src="https://www.giemphotography.com/wp-content/uploads/2020/06/about-pic.jpg" alt="Kim Giem and her husband">
         </div>
 
         <!-- About Text -->
@@ -58,7 +58,7 @@
           <hr>
         </div>
         <?php echo do_shortcode(
-          '[efb_feed fanpage_id="102699908246418" words_limit="25" post_limit="3" skin_id="1680" cache_unit="1" cache_duration="minutes" links_new_tab="1" show_like_box="0"]'
+          '[efb_feed fanpage_id="392580977567305" words_limit="50" post_limit="3" skin_id="1681" cache_unit="30" cache_duration="minutes" links_new_tab="1" show_like_box="0"]'
         ); ?>
       </div>
     </section>
