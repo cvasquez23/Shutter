a12a136cd1183e171ece001f5be3ea5b9a92b442
cvasquez23 <chris@chrisvasquez.dev>
2020-10-13 08:04:56 -0400

    removed recaptcha badge, minor styling edits

--- footer.php
+++ footer.php
@@ -1,36 +1,41 @@
 <?php
 /**
-   * The footer for Shutter
+   * The footer for Giem Photography
    *
-   * @package Shutter
+   * @package Giem Photography
    * @since Shutter Theme 1.0.0
 **/
 ?>
 
       <!-- Footer -->
       <footer class="tag">
-      <div class="container">
-         <div class="row justify-content-center">
-            <a class="back-to-top" href="#home"><i class="fas fa-chevron-up fa-2x fa-up"></i></a><br>
+         <div class="container">
+            <div class="row justify-content-center">
+               <a class="back-to-top" href="#home"><i class="fas fa-chevron-up fa-2x fa-up"></i></a><br>
+            </div>
+            <div class="row justify-content-center">
+               <a class="back-to-top" href="#home">Back to top</a>
+            </div>
+            <div class="row social-footer justify-content-center">
+               <a href="https://www.facebook.com/pg/Kim-Zuidema-Giems-Photography-392580977567305/services/" target="_blank"><i class="fab fa-facebook-square fa-2x"></i></a>
+            <a href="https://www.instagram.com/kim_giem_photography/" target="_blank"><i class="fab fa-instagram fa-2x"></i></a>
+            </div>
+            <div class="row justify-content-center credit">
+               <p>&copy <?php echo date('Y'); ?> Giem Photography</p>
+            </div>
+            <div class="row justify-content-center credit">
+               <p>Developed by
+               <span class="cv">
+                  <a href="https://www.chrisvasquez.dev" target="_blank">Chris Vasquez</a>&nbsp
+               </span>
+               </p>
+            </div>
+            <div class="row justify-content-center">
+               <p class="text-center m-2 privacy">
+               This site is protected by reCAPTCHA and the <a href="https://policies.google.com/privacy"> Google Privacy Policy</a> and <a href="https://policies.google.com/terms">Terms of Service</a> apply.
+               </p>
+            </div>
          </div>
-         <div class="row justify-content-center">
-            <a class="back-to-top" href="#home">Back to top</a>
-         </div>
-         <div class="row social-footer justify-content-center">
-            <a href="https://www.facebook.com/pg/Kim-Zuidema-Giems-Photography-392580977567305/services/" target="_blank"><i class="fab fa-facebook-square fa-2x"></i></a>
-			<a href="https://www.instagram.com/kim_giem_photography/" target="_blank"><i class="fab fa-instagram fa-2x"></i></a>
-         </div>
-         <div class="row justify-content-center credit">
-            <p>&copy <?php echo date('Y'); ?> Giem Photography</p>
-         </div>
-         <div class="row justify-content-center credit">
-            <p>Developed by
-            <span class="cv">
-               <a href="https://www.chrisvasquez.dev" target="_blank">Chris Vasquez</a>&nbsp
-            </span>
-            </p>
-         </div>
-      </div>
       </footer>
 
       <?php wp_footer(); ?>
